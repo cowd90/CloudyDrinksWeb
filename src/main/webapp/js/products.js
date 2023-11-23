0e0a3419a51e0962e8beb90fb25a5915bd1416ac
SunnyBiolie <nguyenthanhdanh1789@gmail.com>
2023-11-24 01:35:05 +0700

    End day 23/11/2023. Done products ui (need to check again)

--- src/main/webapp/js/products.js
+++ src/main/webapp/js/products.js
@@ -1,11 +1,19 @@
+let inputQuantity = $("#prod-quantity");
+let checkInterval;
+let suggestionContainer = $(".suggestion .products_container");
+
+checkDisableDesBtn();
 updateValueOfOrder();
 
+if (suggestionContainer.scrollWidth - suggestionContainer.clientWidth > 0)
+    scrollSuggestionLoop();
+
 function plusQuantity(number) {
-    let inputQuantity = $("#prod-quantity");
     if (inputQuantity.valueAsNumber + number > 0) {
         inputQuantity.valueAsNumber += number;
         $("label[for='prod-quantity']").innerHTML = inputQuantity.valueAsNumber;
     }
+    checkDisableDesBtn();
     updateValueOfOrder();
 }
 
@@ -34,8 +42,46 @@ function updateValueOfOrder() {
             upsizePrice = Number(size.getAttribute("data-prod"));
         }
     });
-    let quantity = $("#prod-quantity").valueAsNumber;
+    let quantity = inputQuantity.valueAsNumber;
     let result = (defaultPrice + upsizePrice) * quantity;
     let formattedPrice = result.toLocaleString("en-US");
     updatePlace.innerHTML = formattedPrice + "đ";
+}
+
+function checkDisableDesBtn() {
+    if (inputQuantity.valueAsNumber === 1)
+        $("#decrease-btn").classList.add("disabled");
+    else
+        $("#decrease-btn").classList.remove("disabled");
+}
+
+function scrollSuggestion(n) {
+    scrollSuggestionLoop();
+
+    let suggestionItemWidth = $(".suggestion .products_container .product").getBoundingClientRect().width;
+    let newScrollLeft = suggestionContainer.scrollLeft + suggestionItemWidth * n;
+
+    let actualScrollSpace = Math.ceil(suggestionContainer.scrollWidth - suggestionContainer.clientWidth);
+
+    if (newScrollLeft > actualScrollSpace)
+        suggestionContainer.scrollTo(0, 0);
+    else if (newScrollLeft < -(suggestionItemWidth/2))
+        suggestionContainer.scrollTo(actualScrollSpace, 0);
+    else
+        suggestionContainer.scrollTo(newScrollLeft, 0);
+}
+function scrollSuggestionLoop() {
+    if (checkInterval) {
+        clearInterval(scrollSuggestionInterval);
+        checkInterval = false;
+
+        interval();
+    } else interval();
+}
+
+function interval() {
+    globalThis.scrollSuggestionInterval = setInterval(() => {
+        $("#next-sug").click();
+    }, 5000);
+    checkInterval = true;
 }
